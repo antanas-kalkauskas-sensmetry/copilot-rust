b33c7cf1f1fc3bc399f2e371b811d2aeba60cdfe
Antanas Kalkauskas <antanas.kalkauskas@sensmetry.com>
2024-03-04 14:32:03 +0200

    started porting Type.hs file

--- copilot-rust/src/Copilot/Compile/Rust/Type.hs
+++ copilot-rust/src/Copilot/Compile/Rust/Type.hs
@@ -1,15 +1,18 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, OverloadedStrings, OverloadedLists #-}
 
 -- | Translate Copilot Core expressions and operators to Rust.
 module Copilot.Compile.Rust.Type
     ( transType
+    , transTypeR
     , transLocalVarDeclType
+    , transLocalVarDeclTypeR
     , transTypeName
     )
   where
 
 -- External imports
 import qualified Language.C99.Simple as C
+import qualified Language.Rust.Syntax as R
 
 -- Internal imports: Copilot
 import Copilot.Core ( Type (..), typeLength, typeName )
@@ -33,6 +36,25 @@ transType ty = case ty of
       len = Just $ C.LitInt $ fromIntegral $ typeLength ty
   Struct s  -> C.TypeSpec $ C.Struct (typeName s)
 
+-- | Translate a Copilot type to a Rust type.
+transTypeR :: Type a -> R.Ty ()
+transTypeR ty = case ty of
+  Bool     -> mkTypeDef "bool"
+  Int8     -> mkTypeDef "i8"
+  Int16    -> mkTypeDef "i16"
+  Int32    -> mkTypeDef "i32"
+  Int64    -> mkTypeDef "i64"
+  Word8    -> mkTypeDef "u8"
+  Word16   -> mkTypeDef "u16"
+  Word32   -> mkTypeDef "u32"
+  Word64   -> mkTypeDef "u64"
+  Float    -> mkTypeDef "f32"
+  Double   -> mkTypeDef "f64"
+  Array _  -> error "Arrays are not implemented yet"
+  Struct _ -> error "Structs are not implemented yet"
+  where
+   mkTypeDef name = R.PathTy Nothing (R.Path False [R.PathSegment name Nothing ()] ()) ()
+
 -- | Translate a Copilot type to a valid (local) variable declaration Rust type.
 --
 -- If the type denotes an array, translate it to a pointer to whatever the
@@ -42,6 +64,10 @@ transType ty = case ty of
 transLocalVarDeclType :: Type a -> C.Type
 transLocalVarDeclType (Array ty') = C.Ptr $ transType ty'
 transLocalVarDeclType ty          = transType ty
+
+transLocalVarDeclTypeR :: Type a -> R.Ty ()
+transLocalVarDeclTypeR (Array _) = error "Arrays are not implemented yet"
+transLocalVarDeclTypeR ty        = transTypeR ty
 
 -- | Translate a Copilot type intro a C typename
 transTypeName :: Type a -> C.TypeName
