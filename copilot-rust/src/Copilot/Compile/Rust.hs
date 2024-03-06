f6529d001eb49bf38e5a74cbf1583ab5d6710b1f
Ricardas Navickas <ricardas.navickas@sensmetry.com>
2024-03-06 16:57:12 +0200

    Remove unused C backend code and move some code from Compile.hs to CodeGen.hs

--- copilot-rust/src/Copilot/Compile/Rust.hs
+++ copilot-rust/src/Copilot/Compile/Rust.hs
@@ -1,11 +1,8 @@
 -- | Compile Copilot specifications to Rust code.
 module Copilot.Compile.Rust
-  ( compile
-  , compileWith
-  , CSettings(..)
-  , mkDefaultCSettings
+  ( mkDefaultCSettings
   ) where
 
 -- Internal imports
-import Copilot.Compile.Rust.Compile  ( compile, compileWith )
+--import Copilot.Compile.Rust.Compile  ( compile, compileWith )
 import Copilot.Compile.Rust.Settings ( CSettings (..), mkDefaultCSettings )
